14f59eac9c22cf50a5ec0634be7709570c7bed1a
nfdeveloper <nfdeveloper04@gmail.com>
2024-11-16 23:29:21 -0300

    Criação da Api

---
+++ src/back/rh_software/src/main/java/io/github/nfdeveloper/rh_software/RhSoftwareApplication.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.rh_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/back/rh_software/src/test/java/io/github/nfdeveloper/rh_software/RhSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nfdeveloper.rh_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
